a7982dcfd978c4bcc499622cbbf6ee528e1fc4c1
spavium <zach_cock@icloud.com>
2026-09-21 13:51:53 +0100

    some bullshit

--- mobile.js
+++ mobile.js
@@ -1,8 +1,8 @@
 function thefunc() { 
 if (screen.width <= 699) {
 document.location = "mobile-version.html";
-document.innerHTML="you are mobile (meaning you shouldnt be here)"
+document.getElementById("tester").innerHTML="you are mobile (meaning you shouldnt be here)"
 }
 if (screen.width >= 700) {
-document.getElementById("tester").innerHTML="you are on pc";}
+document.getElementById("tester").innerHTML="you are on pc (yay!!!!!!)";}
 }
