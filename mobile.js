fd68d39b825cbbd205aaa6467accd5416485a66b
spavium <zach_cock@icloud.com>
2026-09-21 13:08:35 +0100

    briefly learnt js for this bs

---
+++ mobile.js
@@ -0,0 +1,8 @@
+function thefunc() { 
+if (screen.width <= 699) {
+document.location = "mobile-version.html";
+document.innerHTML="you are mobile (meaning you shouldnt be here)"
+}
+if (screen.width >= 700) {
+document.getElementById("tester").innerHTML="you are on pc";}
+}
